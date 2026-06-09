b535b431a960b0e32f0ba38167eb45aae840bea7
“John <john@email.com>
2026-06-09 12:53:18 +0300

    ad first poem

---
+++ main.py
@@ -0,0 +1,18 @@
+from poems import POEM1, POEM2
+from sign import SIGN
+
+author = ("Акім")
+print(SIGN.format(author))
+
+wishes = " написати сайт "
+print(wishes)
+
+stars = "*" * 80
+
+print(stars)
+print(POEM1)
+
+print(stars)
+print(POEM2)
+
+print(stars)
